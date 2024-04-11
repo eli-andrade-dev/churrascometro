adc7db4a44c09257a83734cc476b5598d0242f69
eli-andrade-dev <elisangela.dev@hotmail.com>
2024-04-11 17:44:45 -0300

    adição botão nova consulta

--- src/pagina3/pagina3.js
+++ src/pagina3/pagina3.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Obter valores armazenados na sessionStorage
+    // Pegar os valores armazenados na sessionStorage
     const quantidadeHomens = parseInt(sessionStorage.getItem('quantidadeHomens')) || 0;
     const quantidadeMulheres = parseInt(sessionStorage.getItem('quantidadeMulheres')) || 0;
     const quantidadeCriancas = parseInt(sessionStorage.getItem('quantidadeCriancas')) || 0;
     //const quantidadeBebidas = parseInt(sessionStorage.getItem('quantidadeBebidas')) || 0;
 
-    // Calcular quantidades dos itens
+    // Calcula quantidades dos itens
     const quantidadeCarne = (0.4 * quantidadeHomens) + (0.32 * quantidadeMulheres) + (0.20 * quantidadeCriancas);
     const quantidadePao = (2 * quantidadeMulheres) + quantidadeCriancas;
     const quantidadeCarvao = quantidadeHomens + quantidadeMulheres + quantidadeCriancas;
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const quantidadeAgua = Math.ceil((quantidadeHomens + quantidadeMulheres + quantidadeCriancas) / 5);
     const quantidadeCerveja = 3 * (quantidadeHomens + quantidadeMulheres);
 
-    // Exibir as quantidades na tabela
+    // Exibe as quantidades na tabela
     document.querySelector('.qtdCarne').textContent = quantidadeCarne.toFixed(2) + ' kg';
     document.querySelector('.qtdPao').textContent = quantidadePao;
     document.querySelector('.qtdCarvao').textContent = quantidadeCarvao + ' kg';
@@ -25,7 +25,13 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.qtdAgua').textContent = quantidadeAgua + ' garrafas';
     document.querySelector('.qtdCerveja').textContent = quantidadeCerveja + ' garrafas';
 
-    // Exibir o total de convidados
+    // Exibe o total de convidados
     const totalConvidados = quantidadeHomens + quantidadeMulheres + quantidadeCriancas;
     document.getElementById('convidados').textContent = 'Total de Convidados: ' + totalConvidados;
+});
+
+const novaConsulta = document.querySelector('.novaConsulta');
+
+novaConsulta.addEventListener('click', function() {
+    sessionStorage.clear();
 });
